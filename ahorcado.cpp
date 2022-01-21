27a181255e05263eae9df5534bcf8c2076af915d
Keffisor21 <38363065+Keffisor@users.noreply.github.com>
2022-01-21 04:56:43 +0100

    Update ahorcado.cpp

--- ahorcado.cpp
+++ ahorcado.cpp
@@ -4,10 +4,11 @@
 #include <time.h>
 #include <cstddef>
 #include <cstring>
+#include <string>
 
-static const char* const words[406] = {"alquiler" ,"realeza" ,"instantes" ,"sacerdotes" ,"nocturno" ,"sufragio" ,"v·lida" ,"imprudente" ,"conejo" ,"quinientos" ,"guitarrista" ,"inyecciÛn" ,"recepciÛn" ,"huevo" ,"imperio" ,"nÛmina" ,"narrador" ,"papel" ,"transmitido" ,"merecido" ,"carretera" ,"finalizaciÛn" ,"propuestas" ,"civilizaciÛn" ,"sebastian" ,"territorial" ,"presidenta" ,"dirigente" ,"preciosas" ,"ciudad" ,"conocer" ,"sorprendida" ,"volante" ,"escapar" ,"montÛn" ,"ensalada" ,"deshacer" ,"infeliz" ,"coger" ,"emitir" ,"semblante" ,"silvestre" ,"traje" ,"pulmÛn" ,"traidores" ,"ambulancia" ,"siglas" ,"equipos" ,"malditas" ,"compatible" ,"batido" ,"difusiÛn" ,"fiscales" ,"cazar" ,"coger" ,"tigres" ,"aterrador" ,"oculta" ,"vocalista" ,"conectado" ,"din·mica" ,"anatomÌa" ,"caracol" ,"sesos" ,"estallar" ,"planetas" ,"lagarto" ,"infinita" ,"conejo" ,"reconstrucciÛn" ,"torneo" ,"montado" ,"p·jaro" ,"cliente" ,"peligrosas" ,"territorio" ,"aniversario" ,"pasajes" ,"evaluar" ,"teclado" ,"cuidadoso" ,"prohibiciÛn" ,"rendimiento" ,"alfa" ,"viajero" ,"conversaciÛn" ,"soledad" ,"trigo" ,"salones" ,"escape" ,"plazo" ,"reportaje" ,"tranquilos" ,"obrero" ,"rom·ntica" ,"arquitectura" ,"acelerar" ,"masculino" ,"hermanito" ,"suelto" ,"crimen" ,"imponer" ,"gruesa" ,"grasa" ,"misiÛn" ,"mensaje" ,"rehenes" ,"minerÌa" ,"activar" ,"mitologÌa" ,"estadio" ,"hambre" ,"abrigo" ,"ricas" ,"sorprendida" ,"darnos" ,"sobredosis" ,"activa" ,"almohada" ,"afecto" ,"flaco" ,"Ûrbita" ,"charla" ,"comprometido" ,"muelle" ,"escenario" ,"plata" ,"mensual" ,"prensa" ,"suspendido" ,"valentÌa" ,"cuÒado" ,"inyecciÛn" ,"poderoso" ,"mala" ,"injusto" ,"biografÌa" ,"vena" ,"pequeÒa" ,"integral" ,"actitudes" ,"panda" ,"sol" ,"delegado" ,"superiores" ,"puntaje" ,"reacciones" ,"bate" ,"comercial" ,"disgusto" ,"esclavitud" ,"sesiones" ,"afluente" ,"jinete" ,"contrataciÛn" ,"billetera" ,"doctorado" ,"canciÛn" ,"pariente" ,"reportes" ,"pais" ,"mangas" ,"revertir" ,"creatividad" ,"escultura" ,"viajar" ,"cuidados" ,"tren" ,"orinar" ,"ventana" ,"inundaciÛn" ,"beber" ,"judÌa" ,"cl·sico" ,"aroma" ,"amanecer" ,"metal" ,"juvenil" ,"cumbre" ,"cientÌfico" ,"compensaciÛn" ,"subir" ,"trato" ,"rincÛn" ,"herencia" ,"tubo" ,"enterado" ,"reina" ,"desayunar" ,"plataformas" ,"arena" ,"daÒo" ,"casero" ,"moreno" ,"denunciar" ,"suposiciÛn" ,"gÈnesis" ,"expresar" ,"cantante" ,"inminente" ,"oir" ,"az˙car" ,"ruptura" ,"asesino" ,"voluntad" ,"Ènfasis" ,"enfermo" ,"infraestructura" ,"cobre" ,"regalo" ,"velo" ,"pacientes" ,"permisos" ,"murallas" ,"comunicaciÛn" ,"testigo" ,"consideraciÛn" ,"profundidad" ,"proximidad" ,"calentamiento" ,"aprendiz" ,"salchichas" ,"interrogatorio" ,"sentimental" ,"mandado" ,"usados" ,"carnaval" ,"frio" ,"barato" ,"sana" ,"instancia" ,"porquerÌa" ,"decadencia" ,"cret·cico" ,"simp·tica" ,"vencedor" ,"monstruo" ,"imaginaciÛn" ,"sÌntesis" ,"calidad" ,"costado" ,"rubio" ,"creciendo" ,"bandera" ,"consultar" ,"pandilla" ,"ardiente" ,"asilo" ,"niÒez" ,"prisiÛn" ,"divertida" ,"atento" ,"escenas" ,"regionales" ,"seÒorita" ,"pimienta" ,"palmas" ,"entretenimiento" ,"martillo" ,"progresivo" ,"masaje" ,"fabricaciÛn" ,"port·til" ,"diamante" ,"museo" ,"instrucciÛn" ,"desesperaciÛn" ,"maestrÌa" ,"turno" ,"chofer" ,"luchas" ,"talento" ,"brutal" ,"atraco" ,"sillas" ,"ordinaria" ,"desafortunado" ,"caos" ,"negociaciÛn" ,"finalizaciÛn" ,"tercero" ,"ocultar" ,"limpio" ,"telÈfonos" ,"inferior" ,"autorÌa" ,"herir" ,"resaltar" ,"recto" ,"patriarca" ,"contienda" ,"contraseÒa" ,"relevo" ,"dedicaciÛn" ,"incorrecta" ,"divina" ,"jinete" ,"sumar" ,"votaciÛn" ,"freno" ,"mensaje" ,"meridional" ,"rencor" ,"noche" ,"fresca" ,"p˙blicamente" ,"emisora" ,"parque" ,"fraude" ,"retomar" ,"rito" ,"versos" ,"salÛn" ,"palacio" ,"·tico" ,"ayer" ,"periodista" ,"miÈrcoles" ,"emocionado" ,"emboscada" ,"cabello" ,"disputar" ,"salones" ,"dimisiÛn" ,"sorprendido" ,"futuro" ,"eliminar" ,"desayuno" ,"superficial" ,"corral" ,"taberna" ,"tranvÌa" ,"cifra" ,"cabina" ,"ganador" ,"rana" ,"acelera" ,"oponente" ,"accidente" ,"negociaciones" ,"vender" ,"calvo" ,"intereses" ,"extinciÛn" ,"patear" ,"monje" ,"patrullas" ,"reputaciÛn" ,"soÒar" ,"violenta" ,"rocas" ,"escenario" ,"dinosaurio" ,"azul" ,"confundir" ,"reservas" ,"flaco" ,"alteza" ,"salomÛn" ,"tragedia" ,"tasa" ,"monitor" ,"consejos" ,"queso" ,"error" ,"republicanos" ,"solicitud" ,"puros" ,"honestidad" ,"citados" ,"felicidad" ,"dental" ,"amargo" ,"virreinato" ,"malvado" ,"creativo" ,"suspendido" ,"magia" ,"correcciÛn" ,"filÛsofo" ,"destrozado" ,"elevado" ,"camioneta" ,"reunir" ,"bata" ,"acrÛnimo" ,"sostenible" ,"caballero" ,"invencible" ,"ayudar" ,"mosca" ,"autor" ,"Ûrganos" ,"actriz" ,"marchar" ,"hierba" ,"dibujo" ,"percepciÛn" ,"recreo" ,"ordenadas" ,"resumen" ,"fr·gil" ,"felicitaciones" ,"bancarrota" ,"aire" ,"misterios"};
+static const char* const words[406] = {"alquiler" ,"realeza" ,"instantes" ,"sacerdotes" ,"nocturno" ,"sufragio" ,"v√°lida" ,"imprudente" ,"conejo" ,"quinientos" ,"guitarrista" ,"inyecci√≥n" ,"recepci√≥n" ,"huevo" ,"imperio" ,"n√≥mina" ,"narrador" ,"papel" ,"transmitido" ,"merecido" ,"carretera" ,"finalizaci√≥n" ,"propuestas" ,"civilizaci√≥n" ,"sebastian" ,"territorial" ,"presidenta" ,"dirigente" ,"preciosas" ,"ciudad" ,"conocer" ,"sorprendida" ,"volante" ,"escapar" ,"mont√≥n" ,"ensalada" ,"deshacer" ,"infeliz" ,"coger" ,"emitir" ,"semblante" ,"silvestre" ,"traje" ,"pulm√≥n" ,"traidores" ,"ambulancia" ,"siglas" ,"equipos" ,"malditas" ,"compatible" ,"batido" ,"difusi√≥n" ,"fiscales" ,"cazar" ,"coger" ,"tigres" ,"aterrador" ,"oculta" ,"vocalista" ,"conectado" ,"din√°mica" ,"anatom√≠a" ,"caracol" ,"sesos" ,"estallar" ,"planetas" ,"lagarto" ,"infinita" ,"conejo" ,"reconstrucci√≥n" ,"torneo" ,"montado" ,"p√°jaro" ,"cliente" ,"peligrosas" ,"territorio" ,"aniversario" ,"pasajes" ,"evaluar" ,"teclado" ,"cuidadoso" ,"prohibici√≥n" ,"rendimiento" ,"alfa" ,"viajero" ,"conversaci√≥n" ,"soledad" ,"trigo" ,"salones" ,"escape" ,"plazo" ,"reportaje" ,"tranquilos" ,"obrero" ,"rom√°ntica" ,"arquitectura" ,"acelerar" ,"masculino" ,"hermanito" ,"suelto" ,"crimen" ,"imponer" ,"gruesa" ,"grasa" ,"misi√≥n" ,"mensaje" ,"rehenes" ,"miner√≠a" ,"activar" ,"mitolog√≠a" ,"estadio" ,"hambre" ,"abrigo" ,"ricas" ,"sorprendida" ,"darnos" ,"sobredosis" ,"activa" ,"almohada" ,"afecto" ,"flaco" ,"√≥rbita" ,"charla" ,"comprometido" ,"muelle" ,"escenario" ,"plata" ,"mensual" ,"prensa" ,"suspendido" ,"valent√≠a" ,"cu√±ado" ,"inyecci√≥n" ,"poderoso" ,"mala" ,"injusto" ,"biograf√≠a" ,"vena" ,"peque√±a" ,"integral" ,"actitudes" ,"panda" ,"sol" ,"delegado" ,"superiores" ,"puntaje" ,"reacciones" ,"bate" ,"comercial" ,"disgusto" ,"esclavitud" ,"sesiones" ,"afluente" ,"jinete" ,"contrataci√≥n" ,"billetera" ,"doctorado" ,"canci√≥n" ,"pariente" ,"reportes" ,"pais" ,"mangas" ,"revertir" ,"creatividad" ,"escultura" ,"viajar" ,"cuidados" ,"tren" ,"orinar" ,"ventana" ,"inundaci√≥n" ,"beber" ,"jud√≠a" ,"cl√°sico" ,"aroma" ,"amanecer" ,"metal" ,"juvenil" ,"cumbre" ,"cient√≠fico" ,"compensaci√≥n" ,"subir" ,"trato" ,"rinc√≥n" ,"herencia" ,"tubo" ,"enterado" ,"reina" ,"desayunar" ,"plataformas" ,"arena" ,"da√±o" ,"casero" ,"moreno" ,"denunciar" ,"suposici√≥n" ,"g√©nesis" ,"expresar" ,"cantante" ,"inminente" ,"oir" ,"az√∫car" ,"ruptura" ,"asesino" ,"voluntad" ,"√©nfasis" ,"enfermo" ,"infraestructura" ,"cobre" ,"regalo" ,"velo" ,"pacientes" ,"permisos" ,"murallas" ,"comunicaci√≥n" ,"testigo" ,"consideraci√≥n" ,"profundidad" ,"proximidad" ,"calentamiento" ,"aprendiz" ,"salchichas" ,"interrogatorio" ,"sentimental" ,"mandado" ,"usados" ,"carnaval" ,"frio" ,"barato" ,"sana" ,"instancia" ,"porquer√≠a" ,"decadencia" ,"cret√°cico" ,"simp√°tica" ,"vencedor" ,"monstruo" ,"imaginaci√≥n" ,"s√≠ntesis" ,"calidad" ,"costado" ,"rubio" ,"creciendo" ,"bandera" ,"consultar" ,"pandilla" ,"ardiente" ,"asilo" ,"ni√±ez" ,"prisi√≥n" ,"divertida" ,"atento" ,"escenas" ,"regionales" ,"se√±orita" ,"pimienta" ,"palmas" ,"entretenimiento" ,"martillo" ,"progresivo" ,"masaje" ,"fabricaci√≥n" ,"port√°til" ,"diamante" ,"museo" ,"instrucci√≥n" ,"desesperaci√≥n" ,"maestr√≠a" ,"turno" ,"chofer" ,"luchas" ,"talento" ,"brutal" ,"atraco" ,"sillas" ,"ordinaria" ,"desafortunado" ,"caos" ,"negociaci√≥n" ,"finalizaci√≥n" ,"tercero" ,"ocultar" ,"limpio" ,"tel√©fonos" ,"inferior" ,"autor√≠a" ,"herir" ,"resaltar" ,"recto" ,"patriarca" ,"contienda" ,"contrase√±a" ,"relevo" ,"dedicaci√≥n" ,"incorrecta" ,"divina" ,"jinete" ,"sumar" ,"votaci√≥n" ,"freno" ,"mensaje" ,"meridional" ,"rencor" ,"noche" ,"fresca" ,"p√∫blicamente" ,"emisora" ,"parque" ,"fraude" ,"retomar" ,"rito" ,"versos" ,"sal√≥n" ,"palacio" ,"√°tico" ,"ayer" ,"periodista" ,"mi√©rcoles" ,"emocionado" ,"emboscada" ,"cabello" ,"disputar" ,"salones" ,"dimisi√≥n" ,"sorprendido" ,"futuro" ,"eliminar" ,"desayuno" ,"superficial" ,"corral" ,"taberna" ,"tranv√≠a" ,"cifra" ,"cabina" ,"ganador" ,"rana" ,"acelera" ,"oponente" ,"accidente" ,"negociaciones" ,"vender" ,"calvo" ,"intereses" ,"extinci√≥n" ,"patear" ,"monje" ,"patrullas" ,"reputaci√≥n" ,"so√±ar" ,"violenta" ,"rocas" ,"escenario" ,"dinosaurio" ,"azul" ,"confundir" ,"reservas" ,"flaco" ,"alteza" ,"salom√≥n" ,"tragedia" ,"tasa" ,"monitor" ,"consejos" ,"queso" ,"error" ,"republicanos" ,"solicitud" ,"puros" ,"honestidad" ,"citados" ,"felicidad" ,"dental" ,"amargo" ,"virreinato" ,"malvado" ,"creativo" ,"suspendido" ,"magia" ,"correcci√≥n" ,"fil√≥sofo" ,"destrozado" ,"elevado" ,"camioneta" ,"reunir" ,"bata" ,"acr√≥nimo" ,"sostenible" ,"caballero" ,"invencible" ,"ayudar" ,"mosca" ,"autor" ,"√≥rganos" ,"actriz" ,"marchar" ,"hierba" ,"dibujo" ,"percepci√≥n" ,"recreo" ,"ordenadas" ,"resumen" ,"fr√°gil" ,"felicitaciones" ,"bancarrota" ,"aire" ,"misterios"};
 static int f = 0;
-static char* used;
+std::string used = "";
 
   class Console {
   	public:
@@ -69,41 +70,44 @@ return buff;
 };
 
 void addCharToUsed(char c) {
-size_t n = sizeof(words) / sizeof(const char*);
-used[n] = c;
+	if(used.length() == 0) {
+		used = used + c;
+		return;
+	}
+	used = used + ", " + c;
 }
 
 char detectChar(char c) {
-	if(c == '§') {
+	if(c == '¬§') {
 		}
 	return c;
 }
 void displayUsedWords() {
-size_t n = sizeof(words) / sizeof(const char*);
+int n = used.length();
+
+Console::WriteLine("");
 if(n == 0) {
 	
 } else if(n == 1) {
-	printf("Palabra usada: %c\n", used[0]);
+	printf("Letra usada: %c\n", used[0]);
 } else {
-	Console::Write("Palabras usadas: ");
-	for(int i = 0; i < n; i++) {
-		printf("%c ", used[i]);
-	}
+	Console::Write("Letras usadas: ");
+	Console::Write(used.c_str());
 	Console::WriteLine("");
 }
 }
 
 char convertChar(char c) {
-	if(c == 'È') {
+	if(c == '√©') {
 		return 'e';
 	}
-	if(c == 'Ì') {
+	if(c == '√≠') {
 		return 'i';
 	}
-	if(c == 'Û') {
+	if(c == '√≥') {
 		return 'o';
 	}
-	if(c == '·') {
+	if(c == '√°') {
 		return 'a';
 	}
 	return c;
@@ -236,7 +240,7 @@ bool hasFinished(char* w) {
 		}
      }
      system("cls");
-		Console::WriteLine("°Has ganado!");
+		Console::WriteLine("¬°Has ganado!");
 		return false;
 }
 
@@ -272,7 +276,9 @@ for(int i = 0; i < strlen(word); i++) {
 	for(int i = 0; i < strlen(word); i++) {
 		printf("%c ", w[i]);
 	}
+	
 	Console::WriteLine("");
+	displayUsedWords();
 	Console::WriteLine("");
 	Console::WriteLine("Escriba una letra");
 	char c = Console::readLineChar();
@@ -280,6 +286,9 @@ for(int i = 0; i < strlen(word); i++) {
 	for(int i = 0; i<strlen(w); i++)  {
 		w[i] = n[i];
 	}
+	
+	addCharToUsed(c);
+	
 	system("cls");
 	writeTitle();
 	writePJ(f);
@@ -293,11 +302,12 @@ bool playAgain(const char* word) {
 		printf("%c ", word[i]);
 	}Console::WriteLine("");Console::WriteLine("");
 		if(f == 6) {
-			Console::WriteLine("°Has perdido!");
+			Console::WriteLine("¬°Has perdido!");
 		}
+		used = "";
 		Console::WriteLine("-------------------------");
-Console::WriteLine("øQuieres jugar de nuevo?");
-		Console::WriteLine("[1] SÌ");
+Console::WriteLine("¬øQuieres jugar de nuevo?");
+		Console::WriteLine("[1] S√≠");
 		Console::WriteLine("[0] No");
 		Console::WriteLine("-------------------------");
 		int playAgain = Console::readLineInt();
@@ -306,7 +316,7 @@ Console::WriteLine("øQuieres jugar de nuevo?");
 		} else if(playAgain == 0) {
 			return false;
 		} else {
-			Console::WriteLine("No has escrito un n˙mero de la lista");
+			Console::WriteLine("No has escrito un n√∫mero de la lista");
 			getchar();
 			system("cls");
 			return false;
